405d78098a44d62c62e8ae0888ce1336a9672189
maxvojcuk <maksimvojcuk09@gmail.com>
2024-03-01 21:10:30 +0200

    Зробив повний адаптив  секції Header

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,8 +1,14 @@
 (() => {
   const openBtn = document.querySelector('[data-open_btn]');
+  const closeBtn = document.querySelector('[data-close_btn]');
   const menu = document.querySelector('[data-menu]');
   openBtn.addEventListener('click', toggleMenu);
+  closeBtn.addEventListener('click', closeMenu);
 
+  function closeMenu() {
+    menu.classList.remove('menu-open');
+    document.body.classList.remove('no-scroll');
+  }
   function toggleMenu() {
     menu.classList.toggle('menu-open');
     document.body.classList.toggle('no-scroll');
